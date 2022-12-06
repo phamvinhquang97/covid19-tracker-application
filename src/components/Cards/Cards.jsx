10214f737381e2b0cd30ac50855af494b42cc3b6
phamvinhquang97 <phamvinhquang77@gmail.com>
2022-12-06 23:19:16 +0930

    install UI library and display data in Card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
-
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import styles from './Cards.module.css'
 //Using functional component
 const Cards = () =>{
     
     return (
-        <div>
-            <h1>Cards</h1>
+        <div className={styles.container}>
+            {/* All of Grid item */}
+            <Grid container spacing={3} justify="center">
+                {/* design for one specific grid item */}
+                <Grid item component={Card}>
+                    {/* Card content */}
+                    <CardContent>
+                        {/* Typography - Text on card content */}
+                        <Typography color="textSecondary" gutterBottom>Inferred</Typography>
+                        <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+                        <Typography color= "textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2" >Number of active cases of COVID</Typography>
+                    </CardContent>
+
+                </Grid>
+
+            </Grid>
+            
         </div>
 
-    )
+    );
 }
 
 
