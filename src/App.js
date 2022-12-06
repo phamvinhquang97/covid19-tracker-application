b3d5bde4caf0a6571efb5c317148827a0cda162a
phamvinhquang97 <phamvinhquang77@gmail.com>
2022-12-07 00:03:43 +0930

    get the data from API and finish design interface for Card component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import {Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api';
 class App extends React.Component {
-     
-    // build asynchronous componentDidMound() to get the data.
-    async componentDidMount() {
-        const data = await fetchData();
-        console.log(data);
+    // put all of the field what we want to re-render on interface into state.
+    state = {
+        data: {},
+
 
     }
+    
+    // build asynchronous componentDidMound() to get the data.
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        console.log("this is data: ");
+        console.log(fetchedData);
+
+        // setState is the function to re-render interface.
+        this.setState({data: fetchedData});
+    }
     render() {
+        // this.state.data
+        const {data} = this.state; 
         return(
+            // pass the data on props
             <div className={styles.container}>
                 {/* Card components */}
-                <Cards />
+                <Cards data = {data}/>
                 {/* Chart components */}
                 <Chart />
                 {/* CountryPicker components */}
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,29 +1,65 @@
 import React from 'react'
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
+import {Grid,Card, CardContent, Typography} from '@mui/material/';
 import styles from './Cards.module.css'
+import cx from 'classnames'
+import CountUp from 'react-countup';
+
 //Using functional component
-const Cards = () =>{
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) =>{
+    console.log({lastUpdate});
+    // if we do not get the data
+    if(!confirmed){
+        return 'Loading...'
+    }
     
     return (
         <div className={styles.container}>
             {/* All of Grid item */}
             <Grid container spacing={3} justify="center">
+
                 {/* design for one specific grid item */}
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3}  className = {cx(styles.card, styles.infected)}>
                     {/* Card content */}
                     <CardContent>
                         {/* Typography - Text on card content */}
                         <Typography color="textSecondary" gutterBottom>Inferred</Typography>
-                        <Typography variant="h5" gutterBottom>REAL DATA</Typography>
-                        <Typography color= "textSecondary">REAL DATE</Typography>
+                        <Typography variant="h5" gutterBottom>
+                            {/* Add the Countup component for move number and increase. */}
+                            <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color= "textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2" >Number of active cases of COVID</Typography>
                     </CardContent>
+                </Grid>
 
+                {/* design for one specific grid item */}
+                <Grid item component={Card} xs={12} md={3}  className = {cx(styles.card, styles.recovered)}>
+                    {/* Card content */}
+                    <CardContent>
+                        {/* Typography - Text on card content */}
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5" gutterBottom>
+                            {/* Add the Countup component for move number and increase. */}
+                            <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color= "textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2" >Number of recoveries cases of COVID</Typography>
+                    </CardContent>
+                </Grid>
+
+                {/* design for one specific grid item */}
+                <Grid item component={Card} xs={12} md={3}  className = {cx(styles.card, styles.deaths)}>
+                    {/* Card content */}
+                    <CardContent>
+                        {/* Typography - Text on card content */}
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5" gutterBottom>
+                            {/* Add the Countup component for move number and increase. */}
+                            <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color= "textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography variant="body2" >Number of deaths cases of COVID</Typography>
+                    </CardContent>
                 </Grid>
 
             </Grid>
